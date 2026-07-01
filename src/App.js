f320d50408b98ac77435f28a102a413725986633
Nestor Carvantes <carvantes@gmail.com>
2026-07-01 14:18:45 -0700

    fixed webhook url

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. branch in prod
+          Edit <code>src/App.js</code> and save to reload. branch in prod - fixed webhook url
         </p>
         <a
           className="App-link"
