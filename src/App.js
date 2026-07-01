3ebcbefbd8bb173f4fd39eb8049c83d7e069cd2c
Nestor Carvantes <carvantes@gmail.com>
2026-06-30 17:06:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Git auto deploy!!! regional webhook
+          Edit <code>src/App.js</code> and save to reload. Git auto deploy!!! global webhook
         </p>
         <a
           className="App-link"
